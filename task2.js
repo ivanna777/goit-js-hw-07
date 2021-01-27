6b84d39c1644a92a1705742ebb109bce50076843
Ivanna Kirij <ivannakirij@gmail.com>
2021-01-27 12:25:56 +0200

    прравки

--- task2.js
+++ task2.js
@@ -6,27 +6,34 @@ const ingredients = [
     'Зелень',
     'Приправы',
   ];
-
-  const item1 = document.createElement('li');
-  item1.textContent = ingredients[0];
-
-  const item2 = document.createElement('li');
-  item2.textContent = ingredients[1];
-
-  const item3 = document.createElement('li');
-  item3.textContent = ingredients[2];
-
-  const item4 = document.createElement('li');
-  item4.textContent = ingredients[3];
-
-  const item5 = document.createElement('li');
-  item5.textContent = ingredients[4];
-
-  const item6 = document.createElement('li');
-  item6.textContent = ingredients[5];
-
   const listRef = document.querySelector('#ingredients');
-  listRef.append(item1, item2, item3, item4, item5, item6)
+  const ingFun = ingredients.map(item => {
+      const itemRef = document.createElement('li');
+      itemRef.textContent = item
+      return itemRef
+    });  
+    
+   
+  listRef.append(...ingFun)
 
   console.log(listRef)
+  // const item1 = document.createElement('li');
+  // item1.textContent = ingredients[0];
+
+  // const item2 = document.createElement('li');
+  // item2.textContent = ingredients[1];
+
+  // const item3 = document.createElement('li');
+  // item3.textContent = ingredients[2];
+
+  // const item4 = document.createElement('li');
+  // item4.textContent = ingredients[3];
+
+  // const item5 = document.createElement('li');
+  // item5.textContent = ingredients[4];
+
+  // const item6 = document.createElement('li');
+  // item6.textContent = ingredients[5];
+
+  
 
--- task6.js
+++ task6.js
@@ -2,17 +2,13 @@ const inputRef = document.querySelector('#validation-input')
 console.log(inputRef);
 
 
-inputRef.addEventListener('onblur', e => {
+inputRef.addEventListener('change', e => {
     const inputLength = e.target.value.length;
-    const inputAllowLength = e.target.dataset.length;
+    const inputAllowLength = Number(e.target.dataset.length);
 
     if(inputLength !== inputAllowLength) {
-        inputRef.classList.remove('valid')
         inputRef.classList.add('invalid');
     } else {
-        inputRef.classList.remove('invalid')
-        inputRef.classList.add('valid'); 
-    }
-       
-        console.log(inputRef.classList)   
+        inputRef.classList.add('valid')  
+    }  
 })
